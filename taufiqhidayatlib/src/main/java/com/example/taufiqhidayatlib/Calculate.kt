18a9c38b09c857512f7e34555afba4b5238e17be
ivan siregar <ivan.siregar@gmail.com>
2021-05-21 14:21:57 +0700

    first commit

---
+++ taufiqhidayatlib/src/main/java/com/example/taufiqhidayatlib/Calculate.kt
@@ -0,0 +1,14 @@
+package com.example.taufiqhidayatlib
+
+class Calculate {
+
+    companion object {
+        fun tambah(i1: Int, i2: Int): Int {
+            return (i1 + i2)
+        }
+
+        fun kali(i1: Int, i2: Int): Int {
+            return (i1 * i2)
+        }
+    }
+}
