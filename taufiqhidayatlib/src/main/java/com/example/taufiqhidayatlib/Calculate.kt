70b837ae1f5420985b0c4b9d641517ffe5cba7be
ivan siregar <ivan.siregar@gmail.com>
2021-05-22 09:42:04 +0700

    2021-05-22 commit

--- taufiqhidayatlib/src/main/java/com/example/taufiqhidayatlib/Calculate.kt
+++ taufiqhidayatlib/src/main/java/com/example/taufiqhidayatlib/Calculate.kt
@@ -14,5 +14,9 @@ class Calculate {
         fun sub(i1: Int, i2: Int): Int {
             return (i1 - i2)
         }
+
+        fun kurang(i1: Int, i2: Int): Int {
+            return (i1 - i2)
+        }
     }
 }
